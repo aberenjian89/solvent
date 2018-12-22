28c2672d5e22309cb7ffb767f16cc47ee9f74bbb
Ali Berenjian <ali.rberenjian@gmail.com>
2018-12-21 16:42:18 -0800

    Complete Task

---
+++ front-end/comonents/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export default class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+
+  render(){
+    return(
+      <div>
+        <CssBaseline/>
+        
+      </div>
+    )
+  }
+}
