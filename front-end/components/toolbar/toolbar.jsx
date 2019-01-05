5fc5e40b7f142c289a0b68adc3ead17fa0bd7e43
Alireza Berenjian <Ali.rberenjian@gmail.com>
2019-01-04 18:20:12 -0800

    Add Toolbar Menu

--- front-end/components/toolbar/toolbar.jsx
+++ front-end/components/toolbar/toolbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { Button, IconButton } from '../../../node_modules/@material-ui/core';
  
 
 export default class AppToolbar extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-        open: false
+        open: false,
+        anchorEl: null
     }
+    this.handleMenu = this.handleMenu.bind(this)
+    this.handleClose = this.handleClose.bind(this)
   }
 
   componentWillReceiveProps(nextprops){
@@ -19,12 +25,41 @@ export default class AppToolbar extends React.Component{
     }
   }
 
+  handleMenu(event){
+    debugger
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose(){
+    this.setState({ anchorEl: null });
+  };
+
+
   render(){
+    const open = Boolean(this.state.anchorEl);
     return(
       <div className="appbar">
-        <AppBar color="default" className={!this.state.open ? 'toolbar-close' : 'toolbar-expand' }>
+        <AppBar color="default" className={!this.state.open ? 'toolbar-close' : 'toolbar-expand' } position="relative">
           <Toolbar>
-              
+              <div>
+                <IconButton 
+                aria-owns={open ? 'toolbar-menu' : undefined}
+                aria-haspopup="true"
+                onClick={this.handleMenu}>
+                Account
+                </IconButton>
+                <Menu 
+                id="toolbar-menu"
+                anchorEl={this.state.anchorEl}
+                open={open} 
+                onClose={this.handleClose}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}>
+                  <MenuItem>Profile</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </Menu>
+              </div>
           </Toolbar>
         </AppBar>
       </div>
