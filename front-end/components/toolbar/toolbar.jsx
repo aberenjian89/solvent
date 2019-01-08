5f6630470248bbe8f0a49540569c6c0802d6d234
Ali Berenjian <ali.rberenjian@gmail.com>
2019-01-07 18:34:48 -0800

    Add Account Button

--- front-end/components/toolbar/toolbar.jsx
+++ front-end/components/toolbar/toolbar.jsx
@@ -2,18 +2,32 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
+import { Menu,MenuItem } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
  
 
 const toolbar_style = {
   display: 'flex',
+  justifyContent: 'flex-end'
 }
 
-export default class AppToolbar extends React.Component{
+const styles = theme =>({
+  root:{
+    top: '64px',
+    width: '100px'
+  }
+})
+
+class AppToolbar extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-        open: false
+        open: false,
+        anchorEl: null
     }
+    this.handleClose = this.handleClose.bind(this)
+    this.handleOpen = this.handleOpen.bind(this)
   }
 
   componentWillReceiveProps(nextprops){
@@ -24,15 +38,40 @@ export default class AppToolbar extends React.Component{
     }
   }
 
+  handleOpen(event){
+    this.setState({
+      anchorEl: event.currentTarget
+    })
+  }
+
+  handleClose(){
+    this.setState({
+      anchorEl: null
+    })
+  }
+
   render(){
+    const {anchorEl} = this.state
     return(
       <div className="appbar">
         <AppBar color="default" className={!this.state.open ? 'toolbar-close' : 'toolbar-expand' }>
-          <Toolbar>
-            <div style={toolbar_style}>
-              <Button>
+          <Toolbar style={toolbar_style}>
+            <div>
+              <Button
+                aria-owns={anchorEl ? 'open-menu' : undefined }
+                aria-haspopup="true"
+                onClick={this.handleOpen}>
                   Account
               </Button>
+              <Menu
+                id="open-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={this.handleClose}
+                classes ={{root: classes.root}}>
+                <MenuItem>Settings</MenuItem>
+                <MenuItem>Logout</MenuItem>
+              </Menu>
             </div>
           </Toolbar>
         </AppBar>
@@ -40,3 +79,7 @@ export default class AppToolbar extends React.Component{
     )
   }
 }
+
+
+export default withStyles(styles)(AppToolbar);
+
